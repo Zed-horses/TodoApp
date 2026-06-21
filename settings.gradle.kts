3743c2b15d1b7339b7962a0486b1a3a39814dcbb
Zed-horses <zedhorses6@gmail.com>
2026-06-21 09:04:29 +0200

    settings.gradle.kts

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -13,5 +13,11 @@ dependencyResolutionManagement {
     }
 }
 
+plugins {
+    id("com.android.application") version "8.2.0" apply false
+    id("com.android.library") version "8.2.0" apply false
+    id("org.jetbrains.kotlin.android") version "1.9.10" apply false
+}
+
 rootProject.name = "TodoApp"
 include(":app")
